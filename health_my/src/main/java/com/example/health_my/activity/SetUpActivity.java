d4e344f73f9c64c18ae89bbb52bebe8369094b61
xuanran <xuan@123.com>
2019-12-19 16:41:19 +0800

    修改

--- health_my/src/main/java/com/example/health_my/activity/SetUpActivity.java
+++ health_my/src/main/java/com/example/health_my/activity/SetUpActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -33,7 +34,12 @@ public class SetUpActivity extends WDActivity {
 
     @Override
     protected void initView() {
-        intent(MineActivity.class);
+        setUpActivityRelativeMyMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent(MineActivity.class);
+            }
+        });
     }
 
     @Override
